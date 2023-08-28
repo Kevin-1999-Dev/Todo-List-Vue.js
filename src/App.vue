10ae901e8b820d56759505338638a85369783c05
koekzh <koeshwin9999@gmail.com>
2023-08-28 23:08:00 +0630

    add-keyup

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
   <div id="app">
     <div class="container">
-      <div
-        class="text-white bg-secondary p-3 mb-3 text-center d-flex justify-content-between align-item-center"
-      >
+      <div class="text-white bg-secondary p-3 mb-3 text-center d-flex justify-content-between align-item-center">
         <div class="">
           <h1 class="pt-2">My ToDo List</h1>
         </div>
         <div class="text-end">
           <div class="form-check">
-            <label for="flexCheckChecked" class="form-check-label"
-              >Hide Complete Tasks</label
-            >
-            <input
-              type="checkbox"
-              id="flexCheckChecked"
-              class="form-check-input"
-              v-model="hideComplete"
-            />
+            <label for="flexCheckChecked" class="form-check-label">Hide Complete Tasks</label>
+            <input type="checkbox" id="flexCheckChecked" class="form-check-input" v-model="hideComplete" />
           </div>
           <button class="btn btn-danger d-block mt-2" @click="deleteComplete()">
             Delete Complete Tasks
@@ -30,13 +21,13 @@
       </div>
       <div v-if="this.filterTask.length > 0">
         <div class="row" v-for="(task, index) in filterTask" :key="index">
-        <div class="col-6" :class="task.done ? 'deleteTask' : ''">
-          {{ task.action }}
+          <div class="col-6" :class="task.done ? 'deleteTask' : ''">
+            {{ task.action }}
+          </div>
+          <div class="col-6">
+            <input type="checkbox" v-model="task.done" />
+          </div>
         </div>
-        <div class="col-6">
-          <input type="checkbox" v-model="task.done" />
-        </div>
-      </div>
       </div>
       <div v-else>
         <h3 class="alert alert-warning w-50 text-center">
@@ -47,12 +38,8 @@
       <div class="row">
         <h3>Create Task</h3>
         <div class="col-6">
-          <input
-          type="text"
-          class="form-control"
-          v-model="createTask"
-        />
-        <small class="text-danger" v-if="valiMessage">Please Fill Your task...</small>
+          <input type="text" class="form-control" v-model="createTask" @keyup.enter="addTask()" />
+          <small class="text-danger" v-if="valiMessage">Please Fill Your task...</small>
         </div>
         <div class="col-6">
           <button class="btn btn-secondary" @click="addTask()">Add</button>
@@ -66,7 +53,7 @@
 export default {
   name: "App",
   data: () => ({
-    valiMessage : false,
+    valiMessage: false,
     title: "App",
     createTask: "",
     tasks: [],
@@ -86,19 +73,19 @@ export default {
       localStorage.setItem("Storage", JSON.stringify(this.tasks));
     },
     addTask() {
-      if(this.createTask == null || this.createTask == '' ){
+      if (this.createTask == null || this.createTask == '') {
         this.valiMessage = true;
-      }else{
+      } else {
         this.tasks.push({
           action: this.createTask,
           done: false
         });
-      
+
         this.storage();
-      this.createTask = '';
-      this.valiMessage = false;
+        this.createTask = '';
+        this.valiMessage = false;
       }
-      
+
     },
     deleteComplete() {
       this.tasks = this.tasks.filter((v) => !v.done);
